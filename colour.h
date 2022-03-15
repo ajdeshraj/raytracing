ac776ec80e2e772f3cf3859a16d2923cce514335
ajdeshraj <arjun.deshraj@gmail.com>
2022-03-15 12:10:35 +0530

    Added colour utitity functions

---
+++ colour.h
@@ -0,0 +1,16 @@
+#ifndef COLOUR_H
+#define COLOUR_H
+
+#include "vec3.h"
+
+#include <iostream>
+
+void write_colour(std::ostream &out, colour pixel_colour)
+{
+    // Writing the translated value of each colour component for each pixel
+    out << static_cast<int>(255.99*pixel_colour.x()) << ' '
+        << static_cast<int>(255.99*pixel_colour.x()) << ' '
+        << static_cast<int>(255.99*pixel_colour.x()) << '\n ';
+}
+
+#endif
